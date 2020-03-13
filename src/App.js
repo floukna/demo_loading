41011f0322749079a29e690adfe6899ba439d8e5
Flouk <flouk1150@gmail.com>
2020-03-13 16:00:28 +0700

    Update package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import * as loadingData from "./loading.json";
+import * as successData from "./success.json";
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import "./App.css";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: loadingData.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
+
+const defaultOptions2 = {
+  loop: true,
+  autoplay: true,
+  animationData: successData.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice"
+  }
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => {
+          setLoading(true);
+          setTimeout(() => {
+            setSuccess(true);
+          }, 1000);
+        });
+    }, 1500);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!success ? (
+          <FadeIn>
+            <div style={{ display: "flex" }}>
+              <h1>fetching</h1>
+              {!loading ? (
+                <Lottie options={defaultOptions} height={140} width={140} />
+              ) : (
+                <Lottie options={defaultOptions2} height={140} width={140} />
+              )}
+            </div>
+          </FadeIn>
+        ) : (
+          <h1>Fetch Success</h1>
+        )}
       </header>
     </div>
   );
